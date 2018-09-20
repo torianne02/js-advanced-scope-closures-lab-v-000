943ff0426a2919b0ba743635793985211df636b9
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-20 20:38:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function produceDrivingRange(blockRange) {
+  return function(startingBlock, endingBlock) {
+    let start = parseInt(startingBlock);
+    let end = parseInt(endingBlock);
+    let distance = Math.abs(end - start);
+    let difference = blockRange - distance;
+
+    if (difference > 0) {
+      return `within range by ${difference}`
+    } else {
+      return `${Math.abs(difference)} blocks out of range`
+    };
+  };
+}
+
+function produceTipCalculator(tip) {
+  return function(rideFare) {
+    return rideFare * tip;
+  };
+}
+
+function createDriver() {
+  let driverId = 0;
+
+  return class {
+    constructor(name) {
+      this.id = ++driverId;
+      this.name = name;
+    };
+  };
+}
